38d186d2fd5ad8e2880dc8cbfeab35d6af389a9d
mohansai123 <mohansai3333@gmail.com>
2024-01-10 22:56:24 +0530

    files were added

---
+++ modules/ec2/outputs.tf
@@ -0,0 +1,11 @@
+# ec2-module/outputs.tf
+
+output "instance_ids" {
+  description = "IDs of the created EC2 instances"
+  value       = aws_instance.ec2_instances[*].id
+}
+
+output "public_ip_addresses" {
+  description = "List of public IP addresses associated with EC2 instances"
+  value       = aws_instance.ec2_instances[*].public_ip
+}
---
+++ modules/ec2/variables.tf
@@ -0,0 +1,43 @@
+# ec2-module/variables.tf
+
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  type        = number
+  default     = 3
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "Amazon Machine Image ID"
+  type        = string
+  default     = "ami-05c0f5389589545b7"
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+  default     = "demo-key"
+}
+
+variable "subnet_id" {
+  description = "ID of the subnet where instances will be launched"
+  type        = string
+  default     = "subnet-0e41453c9b1028803"
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs"
+  type        = list(string)
+  default     = []
+}
+
+variable "tags" {
+  description = "A list of maps containing tags for EC2 instances."
+  type        = list(map(string))
+  default     = []
+}
